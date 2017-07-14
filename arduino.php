e63dfd922c838ddd5e5db3a566da4e8b837bbbcd
j14009 <j14009@sangi.jp>
2017-07-14 03:10:54 +0000

    datasyutoku

---
+++ arduino.php
@@ -0,0 +1,16 @@
+<?php
+class Arduino extends AppModel {
+	var $name = 'Arduino';
+	var $validate = array(
+		'light' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
